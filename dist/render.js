adc8718fcf28294d2237c1f874b428755ec3abcf
Your Name <you@example.com>
2019-10-27 14:32:02 +0200

    Added a message for no matching results and improved design

--- dist/render.js
+++ dist/render.js
@@ -1,6 +1,7 @@
 class Renderer {
 
     _renderRecipes(recipes){
+        $("#recipes-container").empty()
         let source = $("#recipe-template").html()
         let template = Handlebars.compile(source)
         let recipesHTML = template({recipes})
